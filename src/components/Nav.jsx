42bd90d9358e6196f5175461f3afcfa4b477ea17
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-09 16:36:08 +0100

    add: nav conponent

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+
+  const menu = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Contact', path: '/contact' }
+  ]
+
+  return <nav>
+    <ul className='flex-link'>
+      {
+        menu.map(({path, label}, i) => {
+          return <li key={i}>
+            <NavLink className='link' to={path}>
+              {({ isActive }) => {
+                return <span className={ isActive ? 'text-blue' : ''}>{
+                  label
+                }</span>
+              }}
+            </NavLink>
+          </li>
+        })
+      }
+    </ul>
+  </nav>
+}
+
+export default Nav
