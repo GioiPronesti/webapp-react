540864d33408dc4d4036df7e650f3915872d4ac4
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-09 17:05:48 +0100

    add: home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+
+
+function HomePage() {
+
+   
+  return <>
+    <section>
+      <div className="container ">
+        <div>
+          <h1 className='heading'>Bool Movies</h1>
+          <h2 className='text-lg'>La galleria dei film popolari in boolean!</h2>
+        </div>
+      </div>
+    </section>
+    <section>
+      elenco dei film
+    </section>
+  </>
+}
+
+export default HomePage
