f7c078e87b00d23cbed94a15ee303f45fe652719
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-09 17:06:03 +0100

    add: home page

---
+++ src/pages/movies/MoviePage.jsx
@@ -0,0 +1,30 @@
+function MoviePage() {
+
+    // recuperare l'id del libro dal path della rotta
+  
+    return <>
+      <section>
+        <div className="container">
+          <h1>Titolo del film</h1>
+          <div>
+            nome regista
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse incidunt cum, minus suscipit saepe unde!
+          </p>
+          {/* media valutazioni con stelline */}
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <h2>Tutte le recensioni</h2>
+        </div>
+        <div className="container">
+          elenco delle recensioni
+        </div>
+      </section>
+      {/* form per nuova recensione */}
+    </>
+  }
+  
+  export default MoviePage;
