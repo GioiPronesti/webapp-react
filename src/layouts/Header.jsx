58cf9ee21ad293400872e2e0245ea46d0370c1f6
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-09 16:37:11 +0100

    add: header component

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,21 @@
+import Nav from "../components/Nav"
+
+
+function Header(){
+
+    return (
+
+    <div className="bg-nav">
+        <section>
+            <div className="container" >
+                <Nav />
+            </div>
+        </section> 
+    
+    </div>
+
+    )
+
+}
+
+export default Header
