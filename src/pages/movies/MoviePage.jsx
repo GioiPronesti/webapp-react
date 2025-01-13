ae7412a5424ded575cfe17ab828feca70e44c990
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-13 16:15:33 +0100

    add: new api for getting movies id details

--- src/pages/movies/MoviePage.jsx
+++ src/pages/movies/MoviePage.jsx
@@ -1,17 +1,45 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 function MoviePage() {
 
     // recuperare l'id del libro dal path della rotta
+
+    const [movie, setMovie] = useState(null)
+
+    const { id } = useParams()
+
+    function fetchMovie() {
+
+      axios.get(`${import.meta.env.VITE_API_URL}/movies/${id}`)
+      .then(res => {
+        setMovie(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+        //  qui ci sarà bisogno di un redirect alla pagina 404
+      })
+    }
+
+    useEffect(() => {
+      fetchMovie()
+    },[id])
   
-    return <>
+    return (
+    movie ? <>
       <section>
         <div className="container">
-          <h1>Titolo del film</h1>
+          <h1>{movie.title}</h1>
           <div>
-            nome regista
+            <img className='width-img' src={movie.image} alt="" />
+          </div>
+          <div>
+            {movie.director}
           </div>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse incidunt cum, minus suscipit saepe unde!
+            {movie.abstract}
           </p>
           {/* media valutazioni con stelline */}
         </div>
@@ -25,7 +53,10 @@ function MoviePage() {
         </div>
       </section>
       {/* form per nuova recensione */}
-    </>
-  }
+      </>
+     : 
+     <div>Loading...</div>
+     )
+}
   
-  export default MoviePage;
+export default MoviePage;
