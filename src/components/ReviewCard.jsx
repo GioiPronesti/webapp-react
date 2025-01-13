20b06db9bd4a545eacf036964081b971f9111c0a
Gioi.Pronesti <gioi.pronesti@gmail.com>
2025-01-13 16:33:41 +0100

    add: Review Card component

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,21 @@
+
+function ReviewCard({ review }){
+  
+    const { name, text, vote} = review
+
+    return (
+        <div className="border-review">
+            <p className="text-small">{text}</p>
+            <div className="flex-vote">
+                <strong>Voto</strong>
+                <div className="vote-rate">{vote}</div>
+            </div>
+            <div className="author-rating">
+                By {name}
+            </div>
+        </div>
+    )
+    
+}
+
+export default ReviewCard
